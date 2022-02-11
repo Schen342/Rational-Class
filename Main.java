65c886fa2b780c4c2f1e4acf4e0538508ebbee5d
Samson Chen <99361667+Schen342@users.noreply.github.com>
2022-02-11 20:31:56 +0000

    I add the 100 point part.

--- Main.java
+++ Main.java
@@ -3,11 +3,47 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter the numerator ----> ");
-    int numerator = input.nextInt();
-    System.out.println("Enter the denominator ----> ");
-    int denominator = input.nextInt();
-    Rational fac = new Rational(numerator, denominator);
-    System.out.println(fac+" equals "+(double)numerator/denominator);
+    System.out.println("Enter the 1st numerator ----> ");
+    int n1 = input.nextInt();
+    System.out.println("Enter the 1st denominator ----> ");
+    int d1 = input.nextInt();
+    System.out.println("Enter the 2nd numerator ----> ");
+    int n2 = input.nextInt();
+    System.out.println("Enter the 2nd denominator ----> ");
+    int d2 = input.nextInt();
+    Rational f1 = new Rational(n1, d1);
+    Rational f2 = new Rational(n2, d2);
+
+    Rational f1m = f1;
+    Rational f2m = f2;
+
+    Rational f1d = f1;
+    Rational f2d = f2;
+
+    Rational f1a = f1;
+    Rational f2a = f2;
+
+    Rational f1s = f1;
+    Rational f2s = f2;
+
+    f1m.multiply(f2m);
+    f1m.getGCF();
+    f1m.simplified();
+    System.out.println(f1 + " * " + f2 + " = "+ f1m);
+
+    f1d.divide(f2d);
+    f1d.getGCF();
+    f1d.simplified();
+    System.out.println(f1 + " / " + f2 + " = "+ f1d);
+
+    f1a.add(f2a);
+    f1a.getGCF();
+    f1a.simplified();
+    System.out.println(f1 + " + " + f2 + " = "+ f1a);
+
+    f1s.subtract(f2s);
+    f1s.getGCF();
+    f1s.simplified();
+    System.out.println(f1 + " - " + f2 + " = "+ f1s);
   }
 }
--- Rational.java
+++ Rational.java
@@ -1,11 +1,15 @@
 class Rational{
     public int numerator;
-
+    public int gcd;
     public int denominator;
 
     public Rational(int n, int d){
         this.numerator = n;
         this.denominator = d;
+    }
+
+    public String getRational(){
+      return numerator +" / "+denominator;
     }
 
     public int getNumerator() {
@@ -37,10 +41,32 @@ class Rational{
     
     public void multiply(Rational other) {
         this.numerator *= other.getNumerator();
-        this.denominator *= other.getDenominator();
+        this.denominator *= other.getDenominator();   
+    }
+
+    public void divide(Rational other) {
+        this.numerator *= other.getDenominator();
+        this.denominator *= other.getNumerator();
     }
     
     public String toString() {
         return this.numerator + " / " + this.denominator;
     }
+
+    public int getGCF(){
+      int gcd = 0;
+      for(int i = 1; i <= numerator && i <= denominator; i++){
+        if(numerator % i == 0 && denominator % i == 0){
+          gcd = i;
+        }
+      }
+      return gcd;
+    }
+
+    public String simplified(){
+      int gcf = getGCF();
+      numerator /= gcf;
+      denominator /= gcf;
+      return numerator+" / "+denominator;
+    } 
 }
